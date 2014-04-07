a08f087dc608a58580fe6b74f45d527c490d7eda
yanniks <kontakt@yanniks.de>
2014-04-07 16:35:12 +0200

    initial CyanDream commit
    Change-Id: Ib2090e319f1a35489908ef2634928c2b1fb3faa4

--- cd.mk
+++ cd.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := Captivate
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/captivatemtd/full_captivatemtd.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := captivatemtd
-PRODUCT_NAME := cm_captivatemtd
+PRODUCT_NAME := cd_captivatemtd
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-I897
 
